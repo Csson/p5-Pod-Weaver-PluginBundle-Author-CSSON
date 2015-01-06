57fee387b7679cb6ab9099ef65d2f6dfd7c6d133
Erik Carlsson <info@code301.com>
2015-01-06 23:06:38 +0100

    Init

---
+++ lib/Pod/Weaver/PluginBundle/Author/CSSON.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Pod::Weaver::PluginBundle::Author::CSSON;
+
+# VERSION
+
+
+
+1;
+
+# ABSTRACT: Short intro
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+  use Pod::Weaver::PluginBundle::Author::CSSON;
+
+=head1 DESCRIPTION
+
+Pod::Weaver::PluginBundle::Author::CSSON is ...
+
+=head1 SEE ALSO
+
+=cut
